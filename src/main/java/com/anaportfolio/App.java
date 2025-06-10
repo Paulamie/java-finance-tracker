9973668a8d8f9aaa5f17ea89614442534a7c1560
paulamie <anapaulatutuzinha@hotmail.com>
2025-06-10 16:26:00 +0100

    Week 1: Created base JavaFX UI and designed DB schema

--- src/main/java/com/anaportfolio/App.java
+++ src/main/java/com/anaportfolio/App.java
@@ -1,13 +1,26 @@
 package com.anaportfolio;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label welcomeLabel = new Label("Welcome to your Personal Finance Tracker!");
+        VBox root = new VBox(20);
+        root.getChildren().add(welcomeLabel);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setTitle("Finance Tracker");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
